6176ecd2d7057f86435974047ca3379dd916889d
Diego Alejandro Robayo cordoba <diegoalrc2006@gmail.com>
2026-07-23 19:43:28 -0500

    Archivo main actualizado para ver el grafo en la página en el json por url

--- src/main.py
+++ src/main.py
@@ -1,10 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
+from servicios.grafo_servicio import GrafoService
 
-app = Flask(__name__, template_folder="frontend")
+app = Flask(__name__)
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+# Ruta para probar el grafo
+@app.route("/grafo")
+def grafo():
+    grafo = GrafoService.grafo_prueba()
+
+    return jsonify(grafo.dictar())
 
 if __name__ == "__main__":
     app.run(debug=True)
