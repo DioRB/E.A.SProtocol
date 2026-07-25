725db64ecd20826b3846b792e194b97669d98db2
JairGo21 <jairgomez.bogota@gmail.com>
2026-07-25 12:59:00 -0500

    Actualizacion de main.py y cambio de nombres

--- src/main.py
+++ src/main.py
@@ -1,7 +1,12 @@
 from flask import Flask, render_template, jsonify
-from app.servicios.grafo_servicio import GrafoService
+from app.servicios.simulador import GrafoService
 
-app = Flask(__name__)
+app = Flask(
+    __name__,
+    template_folder="frontend/templates",
+    static_folder="frontend/static"
+
+)
 
 @app.route("/")
 def index():
@@ -16,3 +21,5 @@ def grafo():
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
