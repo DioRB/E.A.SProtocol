dc71f07425d9afa737e728e3e4a07f8170c743c0
Diego Alejandro Robayo cordoba <diegoalrc2006@gmail.com>
2026-07-25 21:26:41 -0500

    Se hicieron cambios para poder observar visualmente las ediciones hechas al grafo

--- src/frontend/static/js/grafo.js
+++ src/frontend/static/js/grafo.js
@@ -1,91 +1,361 @@
-fetch("/grafo")
-    .then(response => response.json())
-    .then(data => {
+import { crearNodo, eliminarNodo, crearArista, eliminarArista} from "./api.js";
+import { editor } from "./editor.js";
 
-        let elementos = [];
+//Función que dibuja el grafo base
+export function dibujarGrafo(data){
 
-        // Crear nodos
-        data.nodos.forEach(nodo => {
+    let elementos = [];
 
-            elementos.push({
+    data.nodos.forEach(nodo => {
 
-                data: {
-                    id: nodo.id,
-                    label: nodo.etiqueta,
-                    estado: nodo.estado
-                },
+        elementos.push({
 
-                position: {
-                    x: nodo.x,
-                    y: nodo.y
-                }
+            data:{
+                id:nodo.id,
+                label:nodo.etiqueta,
+                estado:nodo.estado
+            },
 
-            });
-
-        });
-
-        // Crear aristas
-        data.aristas.forEach(arista => {
-
-            elementos.push({
-
-                data: {
-                    source: arista.inicio,
-                    target: arista.fin,
-                    label: arista.peso
-                }
-
-            });
-
-        });
-
-        cytoscape({
-
-            container: document.getElementById("cy"),
-
-            elements: elementos,
-
-            style: [
-
-                {
-
-                    selector: "node",
-
-                    style: {
-
-                        "label": "data(label)",
-                        "background-color": "#2E86DE",
-                        "color": "white",
-                        "text-valign": "center",
-                        "text-halign": "center"
-
-                    }
-
-                },
-
-                {
-
-                    selector: "edge",
-
-                    style: {
-
-                        "label": "data(label)",
-                        "width": 3,
-                        "curve-style": "bezier",
-                        "target-arrow-shape": "triangle"
-
-                    }
-
-                }
-
-            ],
-
-            layout: {
-
-                name: "preset"
-
+            position:{
+                x:nodo.x,
+                y:nodo.y
             }
 
         });
 
     });
+
+    data.aristas.forEach(arista=>{
+
+        elementos.push({
+
+            data:{
+                source:arista.inicio,
+                target:arista.fin,
+                label:arista.peso
+            }
+
+        });
+
+    });
+
+    editor.cy = cytoscape({
+
+        container:document.getElementById("cy"),
+
+        elements:elementos,
+
+        style:[
+
+            {
+                selector:"node",
+
+                style:{
+                    label:"data(label)",
+                    "background-color":"#2E86DE",
+                    color:"white",
+                    "text-valign":"center",
+                    "text-halign":"center"
+                }
+
+            },
+
+            {
+
+                selector:"edge",
+
+                style:{
+                    label:"data(label)",
+                    width:3,
+                    "curve-style":"bezier",
+                    "target-arrow-shape":"triangle"
+                }
+
+            },
+
+            {
+            selector: ".seleccionado",
+
+            style:{
+                "background-color":"orange",
+                "border-width":4,
+                "border-color":"red"
+            }
+        }
+
+        ],
+
+        layout:{
+            name:"preset"
+        }
+
+    });
+
+    //Función que crea un nodo al hacer clic
+    editor.cy.on("tap", async function(event){
+
+    if(event.target !== editor.cy){
+        return;
+    }
+
+    switch(editor.modo){
+
+        case "crear_nodo":
+
+            await crearNodoEnPosicion(
+                event.position
+            );
+            break;
+
+    }
+
+});
+    // Función que ajusta las acciones por toque en cada nodo al estar en el modo necesario
+    editor.cy.on("tap", "node", async function(event){
+
+    const nodo = event.target;
+
+    switch(editor.modo){
+
+        // Elimina el nodo
+        case "eliminar":
+            editor.nodoSeleccionado = nodo;
+            eliminarNodoSeleccionado();
+            break;
+        
+        // Selecciona el nodo
+        case "seleccionar":
+            seleccionarNodo(nodo);
+            break;
+
+        // Crea la arista del nodo inicial al final
+        case "crear_arista":
+            crearAristaDesdeNodo(nodo);
+            break;
+    }
+});
+
+    // Función que elimina las arista al darle clic
+    editor.cy.on("tap","edge", async function(event){
+
+        if(editor.modo!="eliminar"){
+            return;
+        }
+        const arista = event.target;
+
+        const confirmar = confirm(
+            "¿Eliminar esta arista?"
+        );
+
+        if(!confirmar){
+            return;
+        }
+
+        await eliminarArista(
+            arista.source().id(),
+            arista.target().id()
+        );
+        arista.remove();
+    }
+);
+
+}
+
+export function obtenerCy(){
+    return editor.cy;
+}
+
+// Crea el nodo en el lugar y pide la información necesaria
+async function crearNodoEnPosicion(posicion){
+
+    const id = prompt("ID del nodo");
+
+    if(!id){
+        return;
+    }
+
+    const etiqueta = prompt(
+        "Etiqueta"
+    ) || id;
+
+    const confianza = Number(
+        prompt(
+            "Nivel de confianza",
+            "1"
+        )
+    );
+
+    const nodo = {
+
+        id:id,
+        etiqueta:etiqueta,
+        estado:"Seguro",
+        nivel_confianza:confianza,
+        x:posicion.x,
+        y:posicion.y
+
+    };
+
+    try{
+        const creado = await crearNodo(nodo);
+        agregarNodoVisual(creado);
+    }
+    catch(error){
+        alert(error);
+    }
+}
+
+// Posiciona el nodo en el lugar
+export function agregarNodoVisual(nodo){
+
+    editor.cy.add({
+        group:"nodes",
+        data:{
+            id:nodo.id,
+            label:nodo.etiqueta,
+            estado:nodo.estado
+        },
+        position:{
+            x:nodo.x,
+            y:nodo.y
+        }
+    });
+}
+
+// Selecciona el nodo
+function seleccionarNodo(nodo){
+
+    if(editor.nodoSeleccionado){
+        editor.nodoSeleccionado.removeClass("seleccionado");
+    }
+    editor.nodoSeleccionado = nodo;
+    editor.nodoSeleccionado.addClass("seleccionado");
+}
+
+// Borra el nodo seleccionado
+async function eliminarNodoSeleccionado(){
+    if(!editor.nodoSeleccionado){
+        return;
+    }
+
+    const confirmar = confirm(
+        "¿Eliminar este nodo?"
+    );
+
+    if(!confirmar){
+        return;
+    }
+
+    await eliminarNodo(
+        editor.nodoSeleccionado.id()
+    );
+
+    editor.nodoSeleccionado.remove();
+    editor.nodoSeleccionado = null;
+
+}
+
+// Crea la arista con inicio y fin seleccionados
+async function crearAristaDesdeNodo(nodo){
+
+    if(editor.origenArista == null){
+
+        editor.origenArista = nodo;
+
+        nodo.addClass("seleccionado");
+        return;
+    }
+
+    if(editor.origenArista.id() == nodo.id()){
+
+        editor.origenArista.removeClass("seleccionado");
+
+        editor.origenArista = null;
+        return;
+    }
+
+    const peso = Number(
+        prompt("Peso")
+    );
+
+    const riesgo = Number(
+        prompt("Riesgo",0)
+    );
+
+    const arista = {
+
+        inicio:editor.origenArista.id(),
+        fin:nodo.id(),
+        peso:peso,
+        riesgo:riesgo
+    };
+
+    try{
+
+        const creada = await crearArista(arista);
+        agregarAristaVisual(creada);
+    }
+
+    catch(error){
+        alert(error.message);
+    }
+
+    editor.origenArista.removeClass("seleccionado");
+    editor.origenArista = null;
+
+}
+
+// Crea la arista visualmente
+function agregarAristaVisual(arista){
+
+    editor.cy.add({
+
+        group:"edges",
+        data:{
+            source:arista.inicio,
+            target:arista.fin,
+            label:arista.peso
+        }
+
+    });
+}
+
+// Actualiza el grafo al base. Borra todos los cambios
+export function actualizarGrafo(data){
+
+    editor.cy.elements().remove();
+
+    const elementos = [];
+
+    data.nodos.forEach(nodo=>{
+
+        elementos.push({
+            group:"nodes",
+            data:{
+                id:nodo.id,
+                label:nodo.etiqueta,
+                estado:nodo.estado
+            },
+            position:{
+                x:nodo.x,
+                y:nodo.y
+            }
+        });
+    });
+
+    data.aristas.forEach(arista=>{
+        elementos.push({
+            group:"edges",
+            data:{
+                source:arista.inicio,
+                target:arista.fin,
+                label:arista.peso
+            }
+        });
+    });
+
+    editor.cy.add(elementos);
+    editor.cy.layout({
+        name:"preset"
+    }).run();
+}
