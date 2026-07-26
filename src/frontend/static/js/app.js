83044c143ba65f5a71c8a6fcd4c198739adbd5ce
Diego Alejandro Robayo cordoba <diegoalrc2006@gmail.com>
2026-07-25 22:39:20 -0500

    Se actualizaron las importaciones y se agregó la carga de nodos a los que se aplica Dijkstra

--- src/frontend/static/js/app.js
+++ src/frontend/static/js/app.js
@@ -1,8 +1,9 @@
 //Archivo principal que lleva todo al html
 import "./interfaz.js";
 import {obtenerGrafo} from "./api.js";
-import {dibujarGrafo, actualizarGrafo} from "./grafo.js";
-import { reiniciarGrafo} from "./api.js";
+import {dibujarGrafo, actualizarGrafo, resaltarRuta} from "./grafo.js";
+import { reiniciarGrafo, ejecutarDijkstra} from "./api.js";
+import {cargarNodosDijkstra} from "./interfaz.js";
 
 async function iniciar(){
 
@@ -10,6 +11,7 @@ async function iniciar(){
 
     dibujarGrafo(grafo);
 
+    cargarNodosDijkstra(grafo);
 }
 
 iniciar();
